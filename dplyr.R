941223bbd29c02f678e55f75bce5882d5b12e5b2
Sherry Zhang <huizezhangsh@gmail.com>
2025-09-03 14:27:09 -0500

    Add files via upload

--- dplyr.R
+++ dplyr.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 library(nycflights13)
 
-# People care the most about delay - can we learn something about it from the data?
+#################################################################################
+#################################################################################
 #DATAT |> filter(PREDICATE)
 
 # basic
@@ -24,6 +25,32 @@ flights$origin |> unique()
 # What goes wrong here?
 flights |> filter(month = 1)
 
+# predicate functions
+1 == 1
+1 == 2
+1 != 2
+3 %in% c(1, 2)
+!(3 %in% c(1, 2))
+(1 == 1) & (1 == 2) # TRUE and FALSE -> FALSE
+(1 == 1) | (1 == 2) # TRUE or FALSE -> TRUE
+
+
+# Your time
+# 1) Find the flights that depart from `JFK`
+flight |> filter(...)
+
+# 2) Find the flights that depart from `JFK` in January
+flight |> filter(...)
+
+# 3) Find the flights that depart from `JFK` or in January
+flight |> filter(...)
+
+# 4) Find the flights that depart from `EWR` and `LGA` and not in January
+flight |> filter(...)
+
+
+#################################################################################
+#################################################################################
 # `mutate()` allows you to
 flights |> mutate(gain = dep_delay - arr_delay)
 flights |>
@@ -36,10 +63,17 @@ flights |>
   mutate(gain = dep_delay - arr_delay) |>
   mutate(speed = distance / (air_time / 60))
 
-flights |> mutate(gain = dep_delay - arr_delay, .before = 1)
-flights |> mutate(gain = dep_delay - arr_delay, .after = day)
-flights |> mutate(gain = dep_delay - arr_delay, .keep = "used")
+# Your time
+# Read the documentation of `mutate()` and modify the following code to put the `gain` column:
+# * as the first column,
+# * after the `day` variable, and
+# * keep only the columns used to create the `gain` column
+flights |> mutate(gain = dep_delay - arr_delay, ...)
+flights |> mutate(gain = dep_delay - arr_delay, ...)
+flights |> mutate(gain = dep_delay - arr_delay, ...)
 
+#################################################################################
+#################################################################################
 # `group_by()` and `summary()`
 flights |> group_by(month)
 flights |>
@@ -61,3 +95,43 @@ flights |>
   summarize(avg_dep_delay = mean(dep_delay, na.rm = TRUE)) |>
   summarize(avg_arr_delay = mean(arr_delay, na.rm = TRUE))
 
+# Your time
+# Apply the `group_by()` + `summarize()` syntax to calculate the average flight distance for each of the three origins.
+
+
+
+#################################################################################
+#################################################################################
+# arrange() by increasing order
+flights |>
+  group_by(carrier) |>
+  summarize(avg_dep_delay = mean(arr_delay, na.rm = TRUE)) |>
+  arrange(avg_dep_delay)
+
+# arrange() by decreasing order
+flights |>
+  group_by(carrier) |>
+  summarize(avg_dep_delay = mean(arr_delay, na.rm = TRUE)) |>
+  arrange(-avg_dep_delay)
+
+# this would also work
+flights |>
+  group_by(carrier) |>
+  summarize(avg_dep_delay = mean(arr_delay, na.rm = TRUE)) |>
+  arrange(desc(avg_dep_delay))
+
+#################################################################################
+#################################################################################
+flights |>
+  mutate(over_2h_delay = dep_delay > 120, .keep = "used")
+
+flights |>
+  mutate(over_2h_delay = dep_delay > 120, .keep = "used") |>
+  group_by(over_2h_delay) |>
+  summarise(n = n())
+
+flights |>
+  mutate(over_2h_delay = dep_delay > 120, .keep = "used") |>
+  group_by(over_2h_delay) |>
+  summarise(n = n()) |>
+  mutate(prop = n/ sum(n) * 100)
